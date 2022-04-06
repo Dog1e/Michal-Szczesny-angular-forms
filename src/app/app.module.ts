df14cf5445721d7c7229158526b1d3433378ca05
Dog1e <michaltablet123@gmail.com>
2022-04-06 16:18:59 +0200

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, TemplateFormComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
