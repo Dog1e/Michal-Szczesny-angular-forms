ba2d262620bc43a946af2d177c358251d8a35a3a
Dog1e <michaltablet123@gmail.com>
2022-04-06 17:43:56 +0200

    2 files modified

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent {
-  data = new FormGroup({});
+  data = new FormGroup({
+    imie: new FormControl('', Validators.required),
+    nazwisko: new FormControl('', [
+      Validators.minLength(3),
+      Validators.required,
+    ]),
+    email: new FormControl('', Validators.required),
+    telefon_s: new FormControl('', Validators.required),
+    kierunkowy: new FormControl('', Validators.required),
+    wewn: new FormControl('', Validators.required),
+    telefon_k: new FormControl('', [
+      Validators.minLength(9),
+      Validators.required,
+    ]),
+    odbiorca: new FormControl('', Validators.required),
+  });
 
-  onSubmit() {}
+  onSubmit() {
+    alert(
+      'Imie: ' +
+        this.data.get('imie').value +
+        '\nNazwisko: ' +
+        this.data.get('nazwisko').value +
+        '\nEmail: ' +
+        this.data.get('email').value +
+        '\nTelefon stacjonarny: ' +
+        ' +' +
+        this.data.get('kierunkowy').value +
+        ' ' +
+        this.data.get('telefon_s').value +
+        ' ' +
+        this.data.get('wewn').value +
+        '\nTelefon komórkowy: ' +
+        this.data.get('telefon_k').value +
+        '\nOdbiorca: ' +
+        this.data.get('odbiorca').value
+    );
+  }
 }
